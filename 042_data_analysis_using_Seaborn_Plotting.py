f7605a277b59872665ec0bdbee6de95aeecf4534
SUBIN S <85066028+Subin-Vidhu@users.noreply.github.com>
2022-08-16 13:42:57 +0530

    a

--- 042_data_analysis_using_Seaborn_Plotting.py
+++ 042_data_analysis_using_Seaborn_Plotting.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-__author__ = "Sreenivas Bhattiprolu"
-__license__ = "Feel free to copy, I appreciate if you acknowledge Python for Microscopists"
+
 
 # https://www.youtube.com/watch?v=ze7HGAf729k
 
