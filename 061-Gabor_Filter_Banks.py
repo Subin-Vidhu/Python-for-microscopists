4b0b7fe1e5471681cf454899a1a99df809d0bc7f
SUBIN S <85066028+Subin-Vidhu@users.noreply.github.com>
2022-08-26 15:55:38 +0530

    Updated 61-Gabor_Filter_Banks

--- 061-Gabor_Filter_Banks.py
+++ 061-Gabor_Filter_Banks.py
@@ -1,17 +1,11 @@
-#!/usr/bin/env python
-__author__ = "Sreenivas Bhattiprolu"
-__license__ = "Feel free to copy, I appreciate if you acknowledge Python for Microscopists"
-
 # https://www.youtube.com/watch?v=BTbIS1mriuY
-
-
 
 import numpy as np
 import cv2
 import pandas as pd
  
 #img = cv2.imread('BSE_Image.jpg')
-img = cv2.imread('images/synthetic.jpg')
+img = cv2.imread('D:/DS_python/Python-for-microscopists/images/synthetic.jpg')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  
 #Here, if you have multichannel image then extract the right channel instead of converting the image to grey. 
 #For example, if DAPI contains nuclei information, extract the DAPI channel image first. 
@@ -33,7 +27,7 @@ for theta in range(2):   #Define number of thetas. Here only 2 theta values 0 an
             for gamma in (0.05, 0.5):   #Gamma values of 0.05 and 0.5
                            
                 gabor_label = 'Gabor' + str(num)  #Label Gabor columns as Gabor1, Gabor2, etc.
-#                print(gabor_label)
+                #print(gabor_label)
                 ksize=9
                 kernel = cv2.getGaborKernel((ksize, ksize), sigma, theta, lamda, gamma, 0, ktype=cv2.CV_32F)    
                 kernels.append(kernel)
@@ -47,3 +41,5 @@ for theta in range(2):   #Define number of thetas. Here only 2 theta values 0 an
 print(df.head())
 
 df.to_csv("Gabor.csv")
+    
+
