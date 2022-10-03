062ef404c0edc106551dd03bf6aea54f89f72d01
SUBIN S <85066028+Subin-Vidhu@users.noreply.github.com>
2022-10-03 15:56:27 +0530

    a5

--- 270-How to deploy your trained machine learning model as an app on Heroku-HAM10000-no docker/main.py
+++ 270-How to deploy your trained machine learning model as an app on Heroku-HAM10000-no docker/main.py
@@ -33,7 +33,7 @@ def getPrediction(filename):
     
     
     #Load model
-    my_model=load_model("model/HAM10000_100epochs.h5")
+    my_model=load_model("D:/DS_python/Python-for-microscopists/270-How to deploy your trained machine learning model as an app on Heroku-HAM10000-no docker/model/HAM10000_100epochs.h5")
     
     SIZE = 32 #Resize to same size as training images
     img_path = 'static/images/'+filename
