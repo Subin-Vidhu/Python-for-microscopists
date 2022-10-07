dc9eff0af11cc72e48ec8dbf5634a120ffd29f00
SUBIN S <85066028+Subin-Vidhu@users.noreply.github.com>
2022-10-07 14:03:33 +0530

    read image

--- 270-How to deploy your trained machine learning model as an app on Heroku-HAM10000-no docker/main_patho.py
+++ 270-How to deploy your trained machine learning model as an app on Heroku-HAM10000-no docker/main_patho.py
@@ -45,239 +45,72 @@ def getPrediction1(filename):
     model1 = load_model('model/weights-improvement-150-0.68', compile=False)
 
 
-    # importing os module
-    import os
-    
-    # Directory
-    #directory = "RC"
-    
-    # Parent Directory path
-    parent_dir_img = "Patchify/"
-    #parent_dir_mask = "D:/Pathology_23_5_22/single_msk_patchify_new/"
-
-    #Remove directories to start as 0
-    """path = os.path.join(parent_dir_img + "*")
-    files = glob.glob(path)
-    for f in files:
-        f = f.replace("//","/")
-        print(f, "f")
-        os.remove(f)"""
-    # Path
-    #path = os.path.join(parent_dir, directory)
-    
-    # Create the directory
-    # 'GeeksForGeeks' in
-    # '/home / User / Documents'
-    #os.mkdir(path)
-    #print("Directory '% s' created" % directory)
-    
-    img_path = parent_dir_img + filename #Parent dir defined above and filename the name of the input image
-    print("Image_Path", img_path)
-    #directory_mask = "A02B74X11_d_mask" #### replace the name of the mask
-    img_path = os.path.join(img_path)
-    os.mkdir(img_path)
-    print("path1", img_path)
-    path1 = "static/images/" + filename
-    tiff_image = tiff.imread(path1)
-    print("tiff_image", tiff_image)
-
-    print("large_image.shape", tiff_image.shape)
-    large_image = cv2.resize(tiff_image, (1792, 768))
-    print("resized_image.shape", large_image.shape)
-    path_pred = "static/Predicted/" + filename.split(".")[0] +".png"
-    print("path_pred", path_pred)
-
-    cv2.imwrite(path_pred, large_image)
-
-    count = 0
-    patches_img = patchify(large_image, (256, 256,3), step=256)
-    for i in range(patches_img.shape[0]):
-        for j in range(patches_img.shape[1]):
-            single_patch_img = patches_img[i,j,:,:]
-            count = count +1
-            tiff.imwrite(img_path + '/image(' + str(count)+ ").tif", single_patch_img)
-            print(count)
-    
-
-
-    ###################### Read Images and Mask from the Patches folder ######       
-
-    #path_RC1 = path2
-
-    path_RC6 = img_path
-
-    train_images = []
-    #train_masks = [] 
-    SIZE = 256
-
-    for i in range(1, 22):
-        print(i)
-        #plt.figure(figsize=(30, 25))
+    #Reading Images RC1
+    test_image_gt_RC1__=nib.load(filename)
+    test_image_gt_RC1_ = test_image_gt_RC1__.get_fdata()
+    print("Read .nii File")
+    test_image_gt_RC1 = test_image_gt_RC1_.transpose(2,1,0)
+    test_image_gt_RC1=scaler.fit_transform(test_image_gt_RC1.reshape(-1, test_image_gt_RC1.shape[-1])).reshape(test_image_gt_RC1.shape)
+    test_image_gt_RC1.shape
+    for i in range(0, len(test_image_gt_RC1)):
+        test_image_gt_RC1[i,:,:] = cv2.flip(test_image_gt_RC1[i,:,:], 0)
         
-        #images
-        #img1 = cv2.imread(path_RC1+"/mask("+str(i)+").tif", 0)
-        #mask = img1
-        #mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
-        #mask = cv2.resize(mask, (SIZE, SIZE), interpolation = cv2.INTER_NEAREST)  
-        #train_masks.append(mask)
-
-
-        img3 = cv2.imread(path_RC6+"/image("+str(i)+").tif", 1)
-        img = img3
-        #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = cv2.resize(img, (SIZE, SIZE))
-        #img = cv2.flip(img, 0)
-        train_images.append(img)
         
-    """if(not np.all(mask == 0)):
-            train_masks.append(mask)
-            train_images.append(img)"""
+    ground_truth_array_test = []
+    img_size = 256
+    for pointer in range(test_image_gt_RC1.shape[0]):
+        image_groundtruth_test = cv2.resize(test_image_gt_RC1[pointer,:,:], (img_size,img_size), cv2.IMREAD_COLOR)
+        ground_truth_array_test.append(image_groundtruth_test)
+        
+    ground_truth_array_test = np.array(ground_truth_array_test)
+    #ground_truth_array_test = np.expand_dims(ground_truth_array_test, 3)
+        
 
 
+    x, y, z = test_image_gt_RC1__.header.get_zooms()
+    x, y = x * 2, y * 2
 
-    train_images = np.array(train_images)
-    #train_masks = np.array(train_masks)
+    preprocessed = []
+    #append slices
 
-    '''   ####### To check Prediction #########
-    import random
-    test_img_number = random.randint(0, len(train_images)-1)
-    test_img = train_images[test_img_number]
-    #ground_truth=train_masks[test_img_number]
-    test_img_input=np.expand_dims(test_img, 0)
+    ###### for testing the performance of the model
+    for i in range(450, 650):
+
+    test_image_gt_one1 = ground_truth_array_test[i,:,:]
+    test_image_gt_one_reshape1 = cv2.resize(test_image_gt_one1,(img_size,img_size), cv2.COLOR_BGR2RGB)
+
+    test_img_input2 = preprocess_input1(test_image_gt_one_reshape1)
+    test_img_input2 = np.expand_dims(test_img_input2, 0)
+    test_pred2 = model2.predict(test_img_input2)
+    test_prediction2 = np.argmax(test_pred2, axis=3)[0,:,:]
+    preprocessed.append(test_prediction2)
+    arr = np.unique(test_prediction2)
+
+    print(i)
     
-    test_img_input1 = preprocess_input1(test_img_input)
-    
-    test_pred1 = model1.predict(test_img_input1)
-    test_prediction1 = np.argmax(test_pred1, axis=3)[0,:,:]
-    
-    
-    plt.figure(figsize=(12, 8))
-    plt.subplot(231)
-    plt.title('Testing Image')
-    plt.imshow(test_img[:,:,0], cmap='gray')
-    #plt.subplot(232)
-    #plt.title('Testing Label')
-    #plt.imshow(ground_truth[:,:,0], cmap='jet')
-    plt.subplot(232)
-    plt.title('Prediction on test image')
-    plt.imshow(test_prediction1, cmap='jet')
+    plt.figure(figsize=(10,10))
+    plt.imshow(test_prediction2, cmap='jet')
+    plt.axis("off")
+    plt.title("%d" %i + str(arr))
+    #plt.savefig(path_RC6+str(i)+".png",bbox_inches='tight')
     plt.show()
-    '''
     
-    
-    ### Reading images to predict and saving it to folder 
-    path_RC6 = "static/Results/"
-    image = filename.split(".")[0]   #### Change the image name ################
-    path3 = os.path.join(path_RC6, image)
-    os.mkdir(path3)
 
 
-    for i in range(0, 21):
-        test_img_number = i
-        test_img = train_images[test_img_number]
-        #ground_truth=train_masks[test_img_number]
-        test_img_input=np.expand_dims(test_img, 0)
-
-        test_img_input1 = preprocess_input1(test_img_input)
-
-        test_pred1 = model1.predict(test_img_input1)
-        test_prediction1 = np.argmax(test_pred1, axis=3)[0,:,:]
-        print(np.unique(test_prediction1))
-
-        #plt.figure(figsize=(20, 15))
-        ''' plt.subplot(231)
-        plt.title('Testing Image')
-        plt.imshow(test_img[:,:,0], cmap='gray')
-        plt.subplot(232)
-        plt.title('Testing Label')
-        plt.imshow(ground_truth[:,:,0], cmap='jet')
-        plt.subplot(233)
-        plt.title('Prediction on test image')'''
-        #plt.axis("off")
-        #plt.imshow(test_prediction1, cmap='jet')
-        cv2.imwrite(path3+"/"+"image("+str(i+1)+").png",test_prediction1*255)
-        #plt.show()
-
-    ############################ Unpatchify  ####################################
-
-    #Apply a trained model on large image, patch by patch
-    path_RC = path3 + "/"
-    predicted_patches = []
-
-    for k in range(1, 22):
-        print(k)
-        #plt.figure(figsize=(30, 25))
-
-        img3 = cv2.imread(path_RC+"image("+str(k)+").png", 0)/255
-        img = img3
-        #img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = cv2.resize(img, (256, 256))
-        #img = cv2.flip(img, 0)
+    preprocessed = np.transpose(np.array(preprocessed), [2, 1, 0])
+    nifty = nib.Nifti1Image(preprocessed, np.diagflat([x, y, z, 1]), dtype=np.int16)
+    nib.save(nifty, filename="D:/ARAMIS/ARAMIS_RENAL/ARAMIS_RENAL_Comparison/comparison_on_RC207/predicted.nii")
         
-        predicted_patches.append(img)
 
-    predicted_patches = np.array(predicted_patches)
-
-    predicted_patches_reshaped = np.reshape(predicted_patches, (3, 7, 256,256) )
-
-
-    path_op = "static/Unpatchified_Result/" 
-    #path_op = os.path.join(path_op,image)
-    #os.mkdir(path_op)
-    reconstructed_image = unpatchify(predicted_patches_reshaped, (768,1792))
-    #plt.figure(figsize=(30, 25))
-    #plt.axis("off")
-    #plt.imshow(reconstructed_image, cmap='gray')
-    cv2.imwrite(path_op + filename.split(".")[0] +".png",reconstructed_image*255)
-    #plt.show()
-
-    #Count 
-    copy1 = cv2.imread(path_op + filename.split(".")[0] +".png")
-
-    img = copy1.copy()
-    #converting to HSV color model
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    #plt.imshow(hsv)
-    print(hsv.shape)
-
-    h, s, v = cv2.split(hsv)
-    #plt.imshow(v)
-
-    _, thr = cv2.threshold(v, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-    #plt.imshow(thr)
-
-    #blurring the image
-    blur = cv2.medianBlur(thr, 5)
-    #plt.imshow(blur)
-
-
-    contours, hierarchy = cv2.findContours(blur,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(copy1, contours, -1, (0,0,255), 3)
-    #plt.imshow(copy1)
-    #print("number of lemons found including error = ", len(contours))
-
-    # arbitarily removing patches having area greater than 1000
-    copy2 = img.copy()
-    count = []
-    for x in contours:
-        area = cv2.contourArea(x)
-        if area > 1000 :
-            count.append(x)
-    cv2.drawContours(copy2, count, -1, (255,0,0), 3)
-    cv2.imwrite("static/Count/"+filename.split(".")[0] +".png",copy2)
-    #print("number of lemons found via contour detection = ", len(count))
-    return len(count)
-    
-
-def getPrediction(filename):
-    print("Hi, I am inside Prediction function!")
-    classes = ['Actinic keratoses', 'Basal cell carcinoma', 
-               'Benign keratosis-like lesions', 'Dermatofibroma', 'Melanoma', 
-               'Melanocytic nevi', 'Vascular lesions']
-    le = LabelEncoder()
-    le.fit(classes)
-    le.inverse_transform([2])
-    
+    def getPrediction(filename):
+        print("Hi, I am inside Prediction function!")
+        classes = ['Actinic keratoses', 'Basal cell carcinoma', 
+                'Benign keratosis-like lesions', 'Dermatofibroma', 'Melanoma', 
+                'Melanocytic nevi', 'Vascular lesions']
+        le = LabelEncoder()
+        le.fit(classes)
+        le.inverse_transform([2])
+        
     
     #Load model
     my_model=load_model("model/HAM10000_100epochs.h5 ")
