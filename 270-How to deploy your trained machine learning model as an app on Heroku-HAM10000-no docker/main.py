c9d92bd1f921c3b04cf1ec2db758e0209203daad
SUBIN S <85066028+Subin-Vidhu@users.noreply.github.com>
2022-10-07 14:34:39 +0530

    Renal model added

--- 270-How to deploy your trained machine learning model as an app on Heroku-HAM10000-no docker/main.py
+++ 270-How to deploy your trained machine learning model as an app on Heroku-HAM10000-no docker/main.py
@@ -42,7 +42,7 @@ def getPrediction1(filename):
 
     #filename = "A31P15X11 a.tif"
     #from keras.models import load_model
-    model1 = load_model('model/weights-improvement-150-0.68', compile=False)
+    model1 = load_model('model/weights-improvement-150-0.68.hdf5', compile=False)
 
 
     #Reading Images RC1
